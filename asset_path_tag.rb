3b5e380633d6b929a9dbc445508f6f5f23fa30c0
gtwiggs <gtwiggs@gmail.com>
2015-03-22 14:00:34 -0500

    Update asset_path_tag.rb
    Support passing a variable in the markup.

--- asset_path_tag.rb
+++ asset_path_tag.rb
@@ -12,21 +12,40 @@
 # /assets/posts/post-title/kitten.png
 # /assets/page-title/pirate.mov
 #
+# Looping example using a variable for the pathname:
+#
+# File _data/image.csv contains:
+#   file
+#   image_one.png
+#   image_two.png
+#
+# {% for image in site.data.images %}{% asset_path {{ image.file }} %}{% endfor %} on post 2015-03-21-post-title
+#
+# Output:
+# /assets/posts/post-title/image_one.png
+# /assets/posts/post-title/image_two.png
+#
 
 module Jekyll
   class AssetPathTag < Liquid::Tag
-    @filename = nil
+    @markup = nil
 
     def initialize(tag_name, markup, tokens)
-      #strip leading and trailing quotes
-      @filename = markup.strip.gsub(/^("|')|("|')$/, '')
+      #strip leading and trailing spaces
+      @markup = markup.strip
       super
     end
 
     def render(context)
-      if @filename.empty?
+      if @markup.empty?
         return "Error processing input, expected syntax: {% asset_path [filename] %}"
       end
+
+      #render the markup
+      rawFilename = Liquid::Template.parse(@markup).render context
+
+      #strip leading and trailing quotes
+      filename = rawFilename.gsub(/^("|')|("|')$/, '')
 
       path = ""
       page = context.environments.first["page"]
@@ -47,7 +66,7 @@ module Jekyll
       path = File.dirname(path) if path =~ /\.\w+$/
 
       #fix double slashes
-      "#{context.registers[:site].config['baseurl']}/assets/#{path}/#{@filename}".gsub(/\/{2,}/, '/')
+      "#{context.registers[:site].config['baseurl']}/assets/#{path}/#{filename}".gsub(/\/{2,}/, '/')
     end
   end
 end
